fc5abc4dde357c04e40af6169203ad17f78ae82d
Centy26 <centrineatieno20@gmail.com>
2023-06-20 04:20:28 -0700

    lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	for (char c = 'a' ; c <= 'z'; c++)
+	_putchar(c);
+
+	_putchar("\n");
+}
