3620dbc3c163260ede5bf37f46aad3a80ba81339
Centy26 <centrineatieno20@gmail.com>
2023-07-03 12:46:44 -0700

    locates char in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,11 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * _strchr - locates a char in a string
  * @s: input
  * c: input
- *Return: NULL
+ *Return: void
  */
 char *_strchr(char *s, char c)
 {
