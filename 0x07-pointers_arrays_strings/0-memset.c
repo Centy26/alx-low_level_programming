eee16bcc55e2b5f9d345bd283dce5233766a8da2
Centy26 <centrineatieno20@gmail.com>
2023-07-03 12:17:10 -0700

    memory set

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,8 +9,9 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	for (unsigned int i = 0; i < n; i++) {
-        s[i] = b;
-    }
-    return s;
+	char *p = s;
+  while (n--) {
+    *p++ = b;
+  }
+  return s;
 }
