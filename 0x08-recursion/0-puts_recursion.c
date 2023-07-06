ff43b4760966f8e04b7bb91d2fe9e1ec6a28c037
Centy26 <centrineatieno20@gmail.com>
2023-07-06 11:40:07 -0700

    recursions

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,12 +8,12 @@
  */
 void _puts_recursion(char *s)
 {
-	if(*s == "\0")
+	if(*s == '\0')
 	{
-		_putchar("\n");
+		_putchar('\n');
 		return;
 	}
 
-	_putchar("%c", *s);
+	_putchar(*s);
 	_puts_recursion(s + 1);
 }
