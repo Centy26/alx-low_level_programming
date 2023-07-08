5680ad1fab0462e0cf1ee3bb85e4dee1e0392dfb
Centy26 <centrineatieno20@gmail.com>
2023-07-08 03:40:01 -0700

    arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: input
+ * @argv: input
+ * Return: void
+ */
+int(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%d\n", argc - 1);
+	return 0;
+}
