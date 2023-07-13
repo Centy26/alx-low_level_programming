0292ff4b9c1049cad40f8d652e83357374e9536a
Centy26 <centrineatieno20@gmail.com>
2023-07-13 12:22:18 -0700

    libraries

---
+++ 0x09-static_libraries/0-strlen.c
@@ -0,0 +1,10 @@
+#include <stddef.h>
+
+int _strlen(char *s) {
+    int len = 0;
+    while (s[len] != '\0') {
+        len++;
+    }
+    return len;
+}
+
---
+++ 0x09-static_libraries/0-strspn.c
@@ -0,0 +1,5 @@
+unsigned int _strspn(char *s, char *accept) {
+    unsigned int count = 0;
+    int i, j;
+    for (i = 0; s[i]
+
